f0fc50471bf49a0594bb3d58ad00e0396480fe08
Pritchad25 <pricts29@gmail.com>
2025-07-12 05:02:09 -0700

    correcting directory structure.

---
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define timestamp
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Run Django shell command to delete inactive customers
+deleted_count=$(python manage.py shell << END
+from django.utils import timezone
+from datetime import timedelta
+from crm.models import Customer
+
+one_year_ago = timezone.now() - timedelta(days=365)
+inactive_customers = Customer.objects.filter(order__isnull=True, created_at__lt=one_year_ago)
+count = inactive_customers.count()
+inactive_customers.delete()
+print(count)
+END
+)
